1594cea60ee1e2e46af71f6be942568ecaf52861
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-04-14 14:24:54 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,10 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-# for pip >= 10
-try:
-    from pip._internal.req import parse_requirements
-# for pip <= 9.0.3
-except ImportError:
-    from pip.req import parse_requirements
-
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 BASE_DIR = path.abspath(path.dirname(__file__))
 
