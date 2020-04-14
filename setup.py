7eb27ce417665ac591f14f977a68446e44ba7336
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-04-14 14:32:32 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-def parse_requirements(filename, session):
+def parse_requirements(filename):
     """ load requirements from a pip requirements file """
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
@@ -32,7 +32,7 @@ req_files = {
 
 requirements = {}
 for req, req_file in req_files.items():
-    reqs = parse_requirements(req_file, session="fake")
+    reqs = parse_requirements(req_file)
     requirements[req] = reqs
 
 setup(
